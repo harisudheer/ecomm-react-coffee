ab34bfc80050836db5e09aefabd06bae186e9c2f
npx <harisudheer@lyrostech.com>
2024-03-14 15:37:32 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
               <Footer />
             </Router> 
           </AppContextProvider> 
-        )}
+        {/* )} */}
       </UserContext.Provider>
     </div>
   );
